90ed0946581b5d4fadf2d06fdbae8ae35c6961eb
Diksha <singhdi@iu.edu>
2023-05-31 08:35:11 -0400

    Added all the Backend Files
    Backend files added

---
+++ Backend Files/Backend/config/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mongoURI: "mongodb+srv://postgres:postgres@cluster0.iqvygrf.mongodb.net/?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    frontend_google_clientID:"973282407747-5e4l7ut9st7c6aqace5d1avdjcjp8o3s.apps.googleusercontent.com",
+    EMAIL_User:"watchaflicks@gmail.com",
+    pswd:"lxwuyakguwqfxqtz",
+    localhost:"http://localhost:3000/api/",
+    phone_number:"MG618ce2e66c0ce521a0bc7578b03ac744",
+    accountSid : 'ACeb5ab04bc6c182e0b79324505645d270',
+    auth_token_twilio :'a8fbcc67f728b5954868b136f42bbebe', //remove this later
+    FB_App_ID: "191905433448640",
+    FB_App_secret: "32e761aaf25b8cc91b7e4218a9c070c3",
+    STRIPE_SECRET_KEY: "sk_test_51MqoAmLu6sgpXpmBCJFyJDe6v1kVJKsgnIGAGn4HpdkbaMJD9ahK0aOPNGuMi5xmui8svWnIhHvvWmU6DJEIwTjE00kX0IMZrr"
+  };
+
+ 
